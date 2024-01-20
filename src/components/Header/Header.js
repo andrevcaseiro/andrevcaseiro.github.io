2542e4fe85ee5868a2ab91b4b32fdb39f1537f77
andrevcaseiro <andre.caseiro02@gmail.com>
2024-01-20 17:36:20 +0000

    Improve header and footer

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,41 @@
 import './Header.css';
-import socialMedia from "../../data/socialMedia.json";
+import header from "../../data/header.json";
 import SocialMediaButton from './SocialMediaButton';
 
+import { ExpandMore } from '@mui/icons-material';
+
 function Header() {
+  function handleScroll() {
+    window.scrollBy({
+      top: 100,
+      behavior: 'smooth',
+    });
+  };
+
   return (
-    <header>
-      <h1>André Caseiro</h1>
+    <header className='mainHeader'>
       <div>
-        {
-          socialMedia.list.map(e => (
-            <SocialMediaButton
-              title={e.title}
-              icon={e.icon}
-              link={e.link}
-            />
-          ))
-        }
+        <div className='info'>
+          <p>Hello, I'm</p>
+          <h1>{header.name}</h1>
+          <div className='socialMedia'>
+            {
+              header.socialMedia.map(e => (
+                <SocialMediaButton
+                  title={e.title}
+                  icon={e.icon}
+                  link={e.link}
+                />
+              ))
+            }
+          </div>
+        </div>
+        <div className='picture'>
+          <img src={header.picture} alt={header.name + " profile picture"} />
+        </div>
+      </div>
+      <div className='scroll-indicator' onClick={handleScroll}>
+        <ExpandMore />
       </div>
     </header>
   );
